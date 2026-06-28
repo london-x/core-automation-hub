2da67033722639e3c12c8e028fcf901884e4e4e9
Artem KiX <kis.artemkis@gmail.com>
2026-06-28 18:55:33 +0300

    (Mini) booster.py

--- (Mini) booster.py
+++ (Mini) booster.py
@@ -35,13 +35,3 @@ m = json.loads(d)
 print(f"OK: {p.name}")
 print(f"CP: {c.name}")
 print(m)
-
-[main 2026-06-28T15:39:37.854Z] StorageMainService: creating application shared storage
-[main 2026-06-28T15:39:37.925Z] [shared storage] Creating shared storage database at 'c:\Users\Dom27\.vscode-shared\sharedStorage\state.vscdb' (wasCreated: false)
-[main 2026-06-28T15:39:37.925Z] [shared storage] Initializing fallback application storage (path: c:\Users\Dom27\AppData\Roaming\Code\User\globalStorage\state.vscdb)
-[main 2026-06-28T15:39:37.941Z] [shared storage] Fallback application storage initialized with 103 items
-[main 2026-06-28T15:39:37.945Z] update#setState idle
-[main 2026-06-28T15:39:38.754Z] vscode-file: Refused to load resource d:\Microsoft VS Code\93cfdd489c\resources\app\extensions\theme-seti\icons\seti.woff from vscode-file: protocol (original URL: vscode-file://vscode-app/d:/Microsoft%20VS%20Code/93cfdd489c/resources/app/extensions/theme-seti/icons/seti.woff)
-(node:12460) [DEP0169] DeprecationWarning: `url.parse()` behavior is not standardized and prone to errors that have security implications. Use the WHATWG URL API instead. CVEs are not issued for `url.parse()` vulnerabilities.
-(Use `Code --trace-deprecation ...` to show where the warning was created)
-[main 2026-06-28T15:40:07.374Z] Extension host with pid 13768 exited with code: 0, signal: unknown.
