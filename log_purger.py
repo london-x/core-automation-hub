c888fb436fc371486702f9f8bb6464bc8b6b1ef2
Artem KiX <kis.artemkis@gmail.com>
2026-06-29 09:23:10 +0300

    log_purger.py

--- log_purger.py
+++ log_purger.py
@@ -4,37 +4,30 @@ import sys
 
 
 def r():
-    """
-    a = [sys.argv] Input terminal command line arguments.
-    p = [path] Target file path object on SSD.
-    f = [file] Internal file stream for disk access.
-    l = [list] Stored database array inside RAM.
-    n = [num] General atomic memory counter for cells.
-    """
-    a = sys.argv
-    if len(a) < 2:
+    args = sys.argv
+    if len(args) < 2:
         print("ERR: NO_FILE")
         sys.exit(1)
-    p = pathlib.Path(a)
-    print(p)
-    if not p.exists():
+    path = pathlib.Path(args)
+    print(path)
+    if not path.exists():
         print("ERR: NOT_FOUND")
         sys.exit(1)
     try:
-        with open(p, "r", encoding="utf-8") as f:
-            l = json.load(f)
-        if not isinstance(l, list):
+        with open(path, "r", encoding="utf-8") as file:
+            list = json.load(file)
+        if not isinstance(list, list):
             raise TypeError
-        n = len(l)
-        print(n)
-        with open(p, "w", encoding="utf-8") as f:
-            json.dump([], f, ensure_ascii=False, indent=4)
+        num = len(list)
+        print(num)
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump([], file, ensure_ascii=False, indent=4)
         print("SUCCESS")
         sys.exit(0)
     except (json.JSONDecodeError, TypeError, Exception):
         print("CRITICAL")
-        with open(p, "w", encoding="utf-8") as f:
-            json.dump([], f, ensure_ascii=False, indent=4)
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump([], file, ensure_ascii=False, indent=4)
         sys.exit(0)
 
 
