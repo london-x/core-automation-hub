cbfb6e5a2b30612a14cdc65156a59125620fcf17
Artem KiX <kis.artemkis@gmail.com>
2026-06-29 09:20:16 +0300

    booster.py

--- booster.py
+++ booster.py
@@ -2,35 +2,25 @@ import sys
 import json
 from pathlib import Path
 
-"""
-a = arguments
-p = path
-f = file
-d = data
-c = copy
-x = copy_file
-m = matrix
-"""
-
-a = sys.argv
-if len(a) < 2:
+args = sys.argv
+if len(args) < 2:
     print("ERR: No file")
     sys.exit(1)
 
-p = Path(a[1])
-if not p.exists():
-    print(f"ERR: {p} not found")
+path = Path(args[1])
+if not path.exists():
+    print(f"ERR: {path} not found")
     sys.exit(1)
 
-with open(p, "r", encoding="utf-8") as f:
-    d = f.read()
+with open(path, "r", encoding="utf-8") as file:
+    data = file.read()
 
-c = p.with_suffix(p.suffix + ".copy")
-with open(c, "w", encoding="utf-8") as x:
-    x.write(d)
+copy = path.with_suffix(path.suffix + ".copy")
+with open(copy, "w", encoding="utf-8") as copy_file:
+    copy_file.write(data)
 
-m = json.loads(d)
+matrix = json.loads(data)
 
-print(f"OK: {p.name}")
-print(f"CP: {c.name}")
-print(m)
+print(f"OK: {path.name}")
+print(f"CP: {copy.name}")
+print(matrix)
