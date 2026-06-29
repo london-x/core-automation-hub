9bfac101366d08ec2c4c56d0c83e5d2ab6c5253a
Artem KiX <kis.artemkis@gmail.com>
2026-06-29 10:26:07 +0300

    log_purger.py

--- log_purger.py
+++ log_purger.py
@@ -8,24 +8,24 @@ def r():
     if len(args) < 2:
         print("ERR: NO_FILE")
         sys.exit(1)
-    path = pathlib.Path(args)
-    print(path)
+    path = pathlib.Path(args[1])
+    print(f"Target path: {path}")
     if not path.exists():
         print("ERR: NOT_FOUND")
         sys.exit(1)
     try:
         with open(path, "r", encoding="utf-8") as file:
-            list = json.load(file)
-        if not isinstance(list, list):
+            content_list = json.load(file)
+        if not isinstance(content_list, list):
             raise TypeError
-        num = len(list)
-        print(num)
+        num = len(content_list)
+        print(f"Elements found: {num}")
         with open(path, "w", encoding="utf-8") as file:
             json.dump([], file, ensure_ascii=False, indent=4)
         print("SUCCESS")
         sys.exit(0)
-    except (json.JSONDecodeError, TypeError, Exception):
-        print("CRITICAL")
+    except (json.JSONDecodeError, TypeError, Exception) as err:
+        print(f"CRITICAL: Wipe triggered due to error ({type(err).__name__})")
         with open(path, "w", encoding="utf-8") as file:
             json.dump([], file, ensure_ascii=False, indent=4)
         sys.exit(0)
